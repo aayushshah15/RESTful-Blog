d2db8706941819145379e065c1776538a27058cf
Aayush Shah <aayush.shah15@gmail.com>
2016-05-08 18:48:21 +0000

    partially added routes

--- app.js
+++ app.js
@@ -5,3 +5,42 @@ var mongoose = require("mongoose");
 
 mongoose.connect("mongodb://localhost/restful-blog");
 app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+
+// Mongoose/model/config
+var blogSchema = new mongoose.Schema({
+    title: String,
+    image: String,
+    body: String,
+    created: {type: Date, default: Date.now}
+});
+
+var Blog = mongoose.model("Blog", blogSchema);
+
+Blog.create({
+    title: "Test Blog",
+    image: "",
+    body: "Hello this is a blog!"
+});
+
+//RESTful routes
+
+app.get("/blogs", function(req, res){
+   Blog.find({}, function(err, blogs){
+       if (err){
+           console.log("Error!");
+       } else {
+           res.render("index", blogs: blogs);
+       }
+   });
+});
+
+app.get("/", function(req,res){
+    res.redirect("/blogs");
+});
+
+app.listen(process.env.PORT, process.env.IP, function(){
+    console.log("Server is running!");
+});
+
